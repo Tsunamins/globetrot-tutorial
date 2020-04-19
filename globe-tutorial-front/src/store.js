9a2afd3b45937c5a9bd85555cee5b83b87b4610e
Tsunamins <reillyamr@gmail.com>
2020-04-19 12:57:41 -0400

    add notes regarding export/import context

--- globe-tutorial-front/src/store.js
+++ globe-tutorial-front/src/store.js
@@ -12,6 +12,11 @@ const reducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 
 //create store takes args: a reducer (not made yet), and the above middleware line
+
+//diff style of export method export const store = below instead of export default store, 
+//changes the way it is imported in index.js as {}
+//this is a good setup when more than one item can be exported, in this case this is the only thing to export
+//why thunk is imported as it is and why react and react-redux items are imported the way they are
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
