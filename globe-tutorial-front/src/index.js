e950c1183d2112e6efa7036ac57bd85881c7e286
Tsunamins <reillyamr@gmail.com>
2020-04-19 12:09:51 -0400

    change html relavant to app, made mock store temp

--- globe-tutorial-front/src/index.js
+++ globe-tutorial-front/src/index.js
@@ -1,13 +1,35 @@
+//original
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// added
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+//somewhat temporary below, to fix error, for now
+//all of below should be incorporated into a more organized fileset outside of
+//here but to demosntrate getting things up and running for now
+const users = () => []
+const reducer = combineReducers({
+  users
+  // shorthand syntax for users: users
+
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+//create store takes args: a reducer (not made yet), and the above middleware line
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>  
+  ,
   document.getElementById('root')
 );
 
