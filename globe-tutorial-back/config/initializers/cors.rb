794ff06ef3fea44ea6917dcfbdb6b3fcf45c83ed
Tsunamins <reillyamr@gmail.com>
2020-04-19 12:09:15 -0400

    remove uneccesary files

--- globe-tutorial-back/config/initializers/cors.rb
+++ globe-tutorial-back/config/initializers/cors.rb
@@ -7,8 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    #origins will be where ever front end is running, local port react is on, or live app whatever somain name is
     origins '*'
 
+    #these would maybe want to change in production for a live api people have access to
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
