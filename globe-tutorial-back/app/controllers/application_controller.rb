7587119f6675fd0afced295ac663f8d9be057a92
Tsunamins <reillyamr@gmail.com>
2020-04-19 10:31:38 -0400

    add mock for current user

--- globe-tutorial-back/app/controllers/application_controller.rb
+++ globe-tutorial-back/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    helpers do 
+        def current_user
+            # mocking a current user temporarily
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end 
+    end 
 end
